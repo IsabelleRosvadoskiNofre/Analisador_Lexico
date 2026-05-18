fbc8b953f08c9ff973eae5621f8b81982b08669c
IsabelleRosvadoskiNofre <rosvadoski37@gmail.com>
2026-05-17 22:26:09 -0300

    Correção dos arquivos de teste

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,13 @@ import java.util.List;
 /*
     Analisador Léxico e Sintático
     Maria Eduarda Mafra e Isabelle Rosvadowski
-    RA: 2553120 e
+    RA: 2553120 e 2553031
  */
 public class Main {
     public static void main(String[] args){
         try{
             //recebe o arquivo de leitura
-            Leitor ler = new Leitor("programa1.gyh");
+            Leitor ler = new Leitor("testes/sintatico/programa1.gyh");
             Parser ps = new Parser(ler);
 
             ps.Programa();
